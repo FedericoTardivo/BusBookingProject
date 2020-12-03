96c5c9d0007ec47e5ff730b63358525112b1df88
FedericoTardivo <federico.tardivo@studenti.unitn.it>
2020-12-03 16:40:12 +0100

    Merged ModelsUpdate into Main

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,12 +1,6 @@
 const express = require('express');
-const {getTickets, getTicket, insertTicket} = require('../controllers/tickets.js');
+const {insertTicket} = require('../controllers/tickets.js');
 const router = express.Router();
-
-/* get method listener for tickets*/
-router.get('/', getTickets);
-
-/* get method listener for ticket with id param*/
-router.get('/:id', getTicket);
 
 /* post method listener to insert tickets */
 router.post('/', insertTicket);
