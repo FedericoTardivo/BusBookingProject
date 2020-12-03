42752076a032eaa507c105ebbea96c11263bb65f
Mattia Birti <mattia.birti@studenti.unitn.it>
2020-12-03 11:24:52 +0100

    done test BookingsReview

--- test/users.test.js
+++ test/users.test.js
@@ -129,107 +129,81 @@ describe ('Test API - User login', () => {
 
 describe ('Test API - BookingReview', () => {
 
-    //dovrebbe essere così
-    it("Get request with wrong parameters should return 400", async () => {
-		const response = await request.get("/api/v1/users/{id}/tickets");
+	const mockUser = new User();
+    mockUser.name = "Mario";
+    mockUser.surname = 'Rossi';
+    mockUser.email = 'mario.rossi@domain.com';
+    mockUser.password = 'MySuperSecretPassword';
+
+	let IDUSER;
+
+    beforeEach(() => {
+        db.users.clear();
+		IDUSER = db.users.register(mockUser);
+	});
+
+    it("Get request with wrong parameters should return 400 (limit test)", async () => {
+
+		const response = await request.get(`/api/v1/users/${IDUSER}/tickets`).query({userId : IDUSER, limit : 0});
 
 		expect(response.status).toBe(400);
 		expect(response.body).toMatchObject({
-			"message": "La richiesta non è valida.",
+			"message": "La richiesta non è valida",
 			"fieldsErrors": [
 				{
-					"fieldName": "email",
-					"fieldMessage": "The field \"email\" must be a valid email address"
-				},
-				{
-					"fieldName": "password",
-					"fieldMessage": "The field \"password\" must be provided"
+					"fieldName": "limit",
+					"fieldMessage": "Il parametro deve essere un numero intero maggiore o uguale a 1"
 				}
 			]
 		});
-    });
-// ancora da implementare
-	it("Get request of a unlogged user should return 401", async () => {
-		const response = await request.get("/api/v1/users/{id}/tickets");
+	});
+
+	it("Get request with wrong parameters should return 400 (offeset test)", async () => {
+
+		const response = await request.get(`/api/v1/users/${IDUSER}/tickets`).query({userId : IDUSER, offset : "abc"});
+
+		expect(response.status).toBe(400);
+		expect(response.body).toMatchObject({
+			"message": "La richiesta non è valida",
+			"fieldsErrors": [
+				{
+					"fieldName": "offset",
+					"fieldMessage": "Il parametro deve essere un numero intero maggiore o uguale a 0"
+				}
+			]
+		});
+	});
+
+	it("Get request of a unlogged user should return 401", async () =>{
+
+		const response = await request.get("/api/v1/users/IDqualsiasi/tickets");
 
 		expect(response.status).toBe(401);
-		expect(response.body).toMatchObject({
-			"message": "La richiesta non è valida.",
-			"fieldsErrors": [
-				{
-					"fieldName": "email",
-					"fieldMessage": "The field \"email\" must be a valid email address"
-				},
-				{
-					"fieldName": "password",
-					"fieldMessage": "The field \"password\" must be provided"
-				}
-			]
-		});
-    });
+		expect(response.body).toBe("Utente non autenticato.");
+	});
 
     it("Get request of a logged user to a private resource should return 403", async () => {
-		/*const response = await request.post("/api/v1/users/{id}/tickets");
 
-		expect(response.status).toBe(400);
-		expect(response.body).toMatchObject({
-			"message": "La richiesta non è valida.",
-			"fieldsErrors": [
-				{
-					"fieldName": "email",
-					"fieldMessage": "The field \"email\" must be a valid email address"
-				},
-				{
-					"fieldName": "password",
-					"fieldMessage": "The field \"password\" must be provided"
-				}
-			]
-		});*/
+		const response = await request.get("/api/v1/users/Qualsiasi/tickets").query({userId : IDUSER});
+
+		expect(response.status).toBe(403);
+		expect(response.body).toBe("Accesso non autorizzato.");
     });
-    
+	
     it("Get request witch correct parameter should return 200", async () => {
-		/*const loginData = {
-			email: "mario.rossi@domain.com",
-			password: "MySuperSecretPassword"
-		};
-		const response = await request.post ("/api/v1/users/{id}/tickets").send(loginData);
 
-        expect(response.status).toBe(200);
-        */
+		const response = await request.get (`/api/v1/users/${IDUSER}/tickets`).query({userId : IDUSER});
+
+		expect(response.status).toBe(200);
+		expect(response.body).toMatchObject({
+			"utente": "id dell'utente",
+			"linea": "linea 5",
+			"fermataPartenza": "Povo Valoni",
+			"fermataArrivo": "Venezia Corallo",
+			"orarioPartenza": "14,30",
+			"orarioArrivo": "14,35"
+		});
+        
     });
     
-/*
-	it("Post request with no registered account", async () =>{
-		const loginData = {
-            email: "notregistered.user@test.com",
-            password: "SomePasswprd"
-        };
-		const response = await request.post("/api/v1/users/login").send(loginData);
-
-		expect(response.status).toBe(401);
-		expect(response.body).toMatchObject({
-			"message": "Utente inserito non è esistente",
-			"fieldsErrors": [
-				{
-					"fieldName": "email",
-					"fieldMessage": "Email does not exist"
-				}
-			]
-		});
-	});
-
-	it("Post request with wrong password", async () => {
-		const loginData = {
-            email: "mario.rossi@domain.com",
-            password: "wrongPassword"
-        };
-		const response = await request.post("/api/v1/users/login").send(loginData);
-
-		expect(response.status).toBe(401);
-		expect(response.body).toMatchObject({
-			"message": "Password errata"
-		});
-	});
-
-	*/
 });
