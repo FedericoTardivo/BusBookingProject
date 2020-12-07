1bd733a7fe85625b4cc9607e05cba47ed62cc341
Mattia Birti <mattia.birti@studenti.unitn.it>
2020-12-07 13:24:44 +0100

    added UI Login

---
+++ UI/scriptLogin.js
@@ -0,0 +1,30 @@
+
+/**
+ * function called by the click of the button 
+ */
+
+function login() {
+
+    var email = document.getElementById("inputEmail").value;
+    var password = document.getElementById("inputPassword").value;
+    
+    // Load the busStops and show them in the table
+    $.ajax({
+        url: "/api/v1/authentication",
+        type: "POST",
+        data: JSON.stringify({email: email, password: password}),
+        contentType: "application/json",
+        dataType: "json"
+    })
+        .done((result) => {
+            sessionStorage.setItem("LoggedUserID", result.id);
+            sessionStorage.setItem("LoggedUserType", result.type);
+            
+            window.location.href="/DashBoard.html";
+        })
+        .fail((jqXHR) => {
+                window.alert(jqXHR.responseJSON.message);
+        });
+}
+
+    
