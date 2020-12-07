9329c607b5c708091b8b8a99f7dce0b30d972ed6
FedericoTardivo <federico.tardivo@studenti.unitn.it>
2020-12-07 10:36:08 +0100

    working...

--- UI/registration.js
+++ UI/registration.js
@@ -2,5 +2,24 @@ function registerUser(){
     $("#formAlert").hide();
     $("#formAlertSuccess").hide();
 
-    console.log("here!");
+    var newUser = {
+        name : $("#userName").val(),
+        surname : $("#userSurname").val(),
+        email : $("#userEmail").val(),
+        password : $("#userPassword").val(),
+        confirmPassword : $("#userConfirmPassword").val()
+    }
+
+    $.ajax({
+        url: "/api/v1/users",
+        type : "POST",
+        data: JSON.stringify(newUser),
+        contentType: "application/json",
+        dataType: "json"
+    }).done((result) => {
+        $("#formAlertSuccess").show();
+    }).fail((jqXHR, textStatus, errorThrown) => {
+        $("#formErrMsg").text(`Risposta del server [${jqXHR.status} - ${errorThrown}]: ${jqXHR.responseText}`);
+        $("#formAlert").show();
+    });
 }
