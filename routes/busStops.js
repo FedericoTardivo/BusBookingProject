54afabd9fd2050eaedd26677127f9c51031883ff
Andrea Concini <andrea.concini@studenti.unitn.it>
2020-12-07 18:18:18 +0100

    Add REST implementation for change/delete bus stops

--- routes/busStops.js
+++ routes/busStops.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
-const {getBusStops, insertBusStop} = require('../controllers/busStops.js');
+const {getBusStops, insertBusStop, updateBusStop, deleteBusStop} = require('../controllers/busStops.js');
 
 const router = express.Router();
 
+// GET: get all the bus stops
 router.get('/', getBusStops);
+
+// POST: Create a new bus stop
 router.post('/', insertBusStop);
+
+// PUT: Update a bus stop
+router.put('/:id', updateBusStop);
+
+// DELETE: Delete a bus stop
+router.delete('/:id', deleteBusStop);
 
 module.exports = router;
