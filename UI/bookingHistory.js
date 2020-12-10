c2d6a4cd8458528c561479042364e3c705fb1848
Mattia Birti <mattia.birti@studenti.unitn.it>
2020-12-10 17:11:32 +0100

    implemented UIRiepilogoPrenotazioni

--- UI/bookingHistory.js
+++ UI/bookingHistory.js
@@ -1,44 +1,14 @@
 const userID = sessionStorage.getItem("LoggedUserID"); //type is yet User 
 var lines = [];
+var stops = [];
 /**
  * function called during the loading of the page bookingHistory
  */
+function loadTable(){
+    loadLines();
+}
+
 function loadTicketBooking() { 
-
-
-    $.ajax({
-        url: `/api/v1/lines`
-    })
-        .done((result) => {
-
-            
-
-        })
-
-        .fail((jqXHR) => {
-            window.alert(jqXHR.responseJSON.message);
-        });
-
-        /**$.ajax({
-            url: `/api/v1/users/${userID}/tickets?` + $.param({userId: userID})
-        })
-            .done((result) => {
-    
-                var table = $("#dataTable tbody");
-                $.each(result, (index, tick) => {
-                    table.append(`<tr>
-                    <td>${tick.issueDate}</td>
-                    <td>${tick.lineId}</td>
-                    <td>${tick.startTime}</td>
-                    <td>${tick.startBusStopId}</td>
-                    <td>${tick.endBusStopId}</td>
-                </tr>`)
-                });
-            })
-    
-            .fail((jqXHR) => {
-                window.alert(jqXHR.responseJSON.message);
-            });**/
 
     $.ajax({
         url: `/api/v1/users/${userID}/tickets?` + $.param({userId: userID})
@@ -47,14 +17,52 @@ function loadTicketBooking() {
 
             var table = $("#dataTable tbody");
             $.each(result, (index, tick) => {
+                var lname = lines.find(l=>l.id == tick.lineId).name;
+                var startname = stops.find(l=>l.id == tick.startBusStopId).name;
+                var stopname = stops.find(l=>l.id == tick.endBusStopId).name;
+
                 table.append(`<tr>
                 <td>${tick.issueDate}</td>
-                <td>${tick.lineId}</td>
+                <td>${lname}</td>
                 <td>${tick.startTime}</td>
-                <td>${tick.startBusStopId}</td>
-                <td>${tick.endBusStopId}</td>
+                <td>${tick.arrivalTime}</td>
+                <td>${startname}</td>
+                <td>${stopname}</td>
+                
             </tr>`)
             });
+        })
+
+        .fail((jqXHR) => {
+            window.alert(jqXHR.responseJSON.message);
+        });
+}
+
+function loadLines(){
+    $.ajax({
+        url: `/api/v1/lines`
+    })
+        .done((result) => {
+
+            lines = result.map(l => {return {id: l._id, name: l.name}})
+            loadBusStops();
+
+        })
+
+        .fail((jqXHR) => {
+            window.alert(jqXHR.responseJSON.message);
+        });
+}
+
+function loadBusStops(){
+    $.ajax({
+        url: `/api/v1/busStops`
+    })
+        .done((result) => {
+
+            stops = result.map(l => {return {id: l.id, name: l.name}})
+            loadTicketBooking();
+
         })
 
         .fail((jqXHR) => {
